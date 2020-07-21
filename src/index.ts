af4153851457b54405dfebe59fefe950fae77667
Kit Jonathan Te <kjonathante@gmail.com>
2020-07-21 15:13:04 -0700

    Added typeorm and modified tsconfig.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// console.log('Hello world!')
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { User } from './entity/User'
+
+const main = async () => {
+  await createConnection()
+  const user = await User.create({
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+  }).save()
+  console.log(user)
+  console.log(await User.find())
+}
+
+main()
